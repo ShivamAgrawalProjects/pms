e143bcc259787e2545f13721c1b9b74b37b251b6
RashiSinghProjects <19singhrashi2000@gail.com>
2020-09-13 13:52:49 +0530

    changes

--- components/test.vue
+++ components/test.vue
@@ -1,11 +1,24 @@
 <template>
     <div>
-         <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand">Navbar</a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
+          <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">LOGO </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Contact <span class="sr-only">(current)</span></a>
+      </li>
+ <li class="nav-item active">
+        <a class="nav-link" href="#">Pricing <span class="sr-only">(current)</span></a>
+      </li>
+ <li class="nav-item active">
+        <a class="nav-link" href="#">Login <span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+    <span class="navbar-text"></span>
+  </div>
 </nav>
     </div>
 </template>
