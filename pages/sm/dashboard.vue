8386ac7aa2179d86eb2673e6fcaf65afe29a49f1
RashiSinghProjects <19singhrashi2000@gail.com>
2020-09-13 16:33:12 +0530

    sm

--- pages/sm/dashboard.vue
+++ pages/sm/dashboard.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
 
+        <form>
+           <div class="row">
+            <div class="col-10">
+                <input type="text"  placeholder="search">
+            </div>
+           <div class="col">
+            <button type="button" class="btn btn-primary" data-toggle="button" aria-pressed="false">Create Project</button>
+           </div>
+           </div>
+        </form>
     </div>
 </template>
 
 <script>
     export default {
-        
+        layout: 'scrummasterlayout',
     }
 </script>
 
