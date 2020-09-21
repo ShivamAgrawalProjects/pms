f1c1181bdc3c31329fcc1bb8be9eb87faea0499e
sumitchauragade <sumeetc1995@gmail.com>
2020-09-21 15:41:54 +0530

    abc

---
+++ pages/forms/blog.vue
@@ -0,0 +1,75 @@
+<template>
+
+    <div class="container" style="background-color: #ffffff;width:80% ;margin:30px 20px 30px 260px">
+       <!-- <a href="#">
+        <i class="fa fa-link"></i>
+        <span>Risk</span>
+      </a> -->
+       <button type="button" class="btn btn-outline-secondary ">Back</button>
+          <form>
+             
+  <div class="form-row">
+    <div class="form-group col-md-8">
+      <label for="inputCity">Requirments:</label>
+      <input type="text" class="form-control" id="inputReason">
+    </div>
+    
+  </div>
+
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label >Start Date:</label>
+      <input type="date" class="form-control" id="inputReason">
+    </div>
+     <div class="form-group col-md-4">
+      <label > End Date:</label>
+      <input type="date" class="form-control" id="inputReason">
+    </div>
+     </div>
+
+  <div class="form-row">
+    <div class="form-group col-md-4">
+      <label for="inputState">Priority</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+
+    
+    
+    <div class="form-group col-md-4">
+      <label for="inputState">Description</label>
+      <input type="text" class="form-control" id="inputReason">
+    </div>
+  </div>
+
+  <form>
+  <div class="form-group">
+  
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+</form>
+  
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+</template>
+
+<script>
+    export default {
+        layout: 'scrummasterlayout',
+        
+    }
+</script>
+
+<style >
+.container1{
+  
+  box-shadow: 500px 500px 5px lightgrey;
+  padding:40px;
+    text-align: center;
+
+}
+</style>
